3a5e21b7f017280a38d15eed3a8e105b2d2c5e9b
keviny <kevinyih2022@gmail.com>
2025-04-27 01:20:18 -0700

    update case, because it's case sensitive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-import HomeProperties from "@/components/Homeproperties";
+import HomeProperties from "@/components/HomeProperties";
 import connectDB from "../../config/database";
 import Logotest from "@/components/Logotest";
 
